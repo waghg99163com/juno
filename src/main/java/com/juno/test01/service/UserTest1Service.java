ab464a77c5948115ed1fad1f6fe35b2be5fc17b4
wangchuang <01733751@yto.net.cn>
2019-07-23 17:34:03 +0800

    加了事务

--- src/main/java/com/juno/test01/service/UserTest1Service.java
+++ src/main/java/com/juno/test01/service/UserTest1Service.java
@@ -6,15 +6,21 @@ import com.juno.test02.dao.UserMapper2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class UserTest1Service {
 
     @Autowired(required = false)
     private UserMapper1 userMapper1;
 
+    @Transactional
     public int insertTest(User user)
     {
-        return userMapper1.insertTest(user.getName(),user.getAge());
+
+        int result = userMapper1.insertTest(user.getName(),user.getAge());
+        int i = 1/0;
+        return result;
     }
 
     public User selectTest(Long id)
