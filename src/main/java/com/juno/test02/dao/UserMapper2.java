d33aeba4f98b066f0aca05c41240b6a70ad9a089
chuang.wang <chuang.wang@shuyun.com>
2019-07-24 00:01:38 +0800

    加上@Param

--- src/main/java/com/juno/test02/dao/UserMapper2.java
+++ src/main/java/com/juno/test02/dao/UserMapper2.java
@@ -3,12 +3,13 @@ package com.juno.test02.dao;
 import com.juno.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper2 {
     @Insert("insert into user (NAME, AGE) value (#{name},#{age})")
-    Integer insertTest(String name, Long age);
+    Integer insertTest(@Param("name") String name, @Param("age") Long age);
 
     @Select("select * from user where id = #{id}")
     User selectTest(Long id);
