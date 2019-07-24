f9132412669c1e8c54bad47823dafea1a781572f
wangchuang <01733751@yto.net.cn>
2019-07-24 10:52:42 +0800

    更改事务引用包

--- src/main/java/com/juno/test02/service/UserTest2Service.java
+++ src/main/java/com/juno/test02/service/UserTest2Service.java
@@ -5,6 +5,9 @@ import com.juno.test01.dao.UserMapper1;
 import com.juno.test02.dao.UserMapper2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class UserTest2Service {
@@ -12,6 +15,7 @@ public class UserTest2Service {
     @Autowired(required = false)
     private UserMapper2 userMapper2;
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public int insertTest(User user)
     {
         return userMapper2.insertTest(user.getName(),user.getAge());
