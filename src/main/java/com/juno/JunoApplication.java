37d0a4571f81efd22b3370609c9c9559ea08b04d
chuang.wang <chuang.wang@shuyun.com>
2019-09-20 00:35:01 +0800

    简单的定时任务

--- src/main/java/com/juno/JunoApplication.java
+++ src/main/java/com/juno/JunoApplication.java
@@ -8,12 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //不加该注解的话会报没有DBConfig1这个bean的错
 @EnableConfigurationProperties(value = {DBConfig1.class,DBConfig2.class})
 //@EnableJpaRepositories
 @MapperScan("com.juno.mapper")
+@EnableScheduling
 public class JunoApplication {
 
     public static void main(String[] args) {
